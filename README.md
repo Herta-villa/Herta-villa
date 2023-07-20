6f902cf3f7e82438a27a95d3452687608ab67d7f
MingxuanGame <MingxuanGame@outlook.com>
2023-07-20 18:49:11 +0800

    :sparkles: 支持查询光锥遗器攻略

--- README.md
+++ README.md
@@ -5,3 +5,9 @@
 ## 交流
 
 前往大别野「斩尽芜杂」（`aaUeZqd`）
+
+## 许可
+
+Herta-villa 使用 AGPL-3.0 许可证
+
+Herta-villa 遵循所使用的资源库 [Mar-7th/StarRailRes](https://github.com/Mar-7th/StarRailRes) 的 AGPL-3.0 许可
--- herta/plugins/guide.py
+++ herta/plugins/guide.py
@@ -1,8 +1,11 @@
 from __future__ import annotations
 
+import contextlib
 import re
 
 from herta.bot import get_bot
+from herta.exception import UploadFailed
+from herta.log import logger
 from herta.utils.cache import Cache
 from herta.utils.index.build_index import Builder
 from herta.utils.index.index import Index
@@ -15,10 +18,12 @@ from hertavilla import (
     VillaBot,
     get_backend,
 )
+from hertavilla.exception import CallingApiException
 
 backend = get_backend()
 char_cache = Cache("character_guide")
-
+light_cone_cache = Cache("light_cone_guide")
+relic_sets_cache = Cache("relic_set_guide")
 
 bot = get_bot()
 regex = r"/星穹攻略 (.+)"
@@ -26,10 +31,56 @@ regex = r"/星穹攻略 (.+)"
 
 @backend.on_startup
 async def build_index() -> None:
-    await Builder.run_build("characters")
+    await Builder.run_build("characters", "light_cones", "relic_sets")
 
 
-index = Index("characters")
+char_index = Index("characters")
+light_cones_index = Index("light_cones")
+relic_sets_index = Index("relic_sets")
+
+CHAR_SEARCH = ("角色", char_index, char_cache, 1920, 3000)
+LIGHT_CONE_SEARCH = ("光锥", light_cones_index, light_cone_cache, 1920, 1080)
+RELIC_SEARCH = ("遗器", relic_sets_index, relic_sets_cache, 1800, 720)
+
+
+async def _img_cache(
+    bot: VillaBot,
+    villa_id: int,
+    path: str,
+    cache: Cache,
+) -> str:
+    if cache[path]:
+        return cache[path]
+    for i in range(4):
+        if i > 0:
+            logger.warning(f"上传图片失败，正在重试 {i}/3")
+        with contextlib.suppress(CallingApiException):
+            img = cache[path] = await bot.transfer_image(
+                villa_id,
+                sr_res(f"/{path}"),
+            )
+            return img
+    raise UploadFailed
+
+
+async def _get_guide(name: str, bot: VillaBot, villa_id: int) -> Image | None:
+    for search in (CHAR_SEARCH, LIGHT_CONE_SEARCH, RELIC_SEARCH):
+        category, index, cache, width, height = search
+        logger.opt(colors=True).info(
+            f"正在 <y>{category}</y> 中搜索 <green>{name}</green>...",
+        )
+        with contextlib.suppress(ValueError):
+            index.set_id_by_name(name)
+            logger.opt(colors=True).success(
+                f"已在 <y>{category}</y> 中找到 <green>{name}</green>",
+            )
+            path = index["guide_overview"][0]  # 听语惊花
+            return Image(
+                await _img_cache(bot, villa_id, path, cache),
+                width,
+                height,
+            )
+    return None
 
 
 @bot.regex(regex)
@@ -37,19 +88,12 @@ async def _(event: SendMessageEvent, bot: VillaBot):
     name = re.match(regex, event.message.plaintext)
     if not name:
         return
-    chain = MessageChain()
     try:
-        index.set_id_by_name(name[1])
-    except ValueError:
-        chain.append("没找到角色攻略，是不是你名称输错了呢~")
-    else:
-        path = index["guide_overview"][0]  # Nwflower
-        if char_cache[path]:
-            img = char_cache[path]
-        else:
-            img = char_cache[path] = await bot.transfer_image(
-                event.villa_id,
-                sr_res(f"/{path}"),
-            )
-        chain.append(Image(img, width=1920, height=3000))
+        msg = (
+            await _get_guide(name[1], bot, event.villa_id)
+            or "未找到相关内容，请检查是否输入有误~"
+        )
+    except UploadFailed:
+        msg = "出现错误: 图片上传失败。请重试"
+    chain = MessageChain(msg)
     await bot.send(event.villa_id, event.room_id, chain)
